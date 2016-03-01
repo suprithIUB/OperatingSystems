4ad5a452a4d7ff9e21c3a4b22e50a4806b7106a1
Suprith <suprchan@indiana.edu>
2016-03-01 12:12:09 -0500

    working without cond var

--- apps/babyBird.c
+++ apps/babyBird.c
@@ -7,22 +7,18 @@ void babyBirdProcess(int babyId){
 	while(1){
 		printf("Inside baby bird:");
 		mutex_lock(&baby_mutex); //do nothing, wait for the lock to be false
-		//critical section
-		//printf("Inside baby bird: %d", worms);
-		//while(worms <= 0)		
-		//	cond_wait(cv_worms, &baby_mutex);
+		
 		worms--;
-		printf("I'm a baby bird: %d, worm count: %d", babyId, worms);
-		//sleep(1000);
+		printf("I'm a baby bird: %d, worm count: %d\n", babyId, worms);
+		
 		if(worms <= 0){
 			
-		//printf("I'm a baby bird: %d, worm count: %d\n", babyId, worms);
-			//cond_signal(cv_empty);
+		
 			mutex_unlock(&parent_mutex);
 		}
 		else
 			mutex_unlock(&baby_mutex); //release lock
 	}
-	//kill(getpid());
+	
 }
 
--- apps/parentBird.c
+++ apps/parentBird.c
@@ -6,19 +6,12 @@
 void parentBirdProcess(){	
 	int wormsCopy = worms;	
 	while(parent_itr > 0){	
-		printf("I'm a parent bird: %d\n", worms);
 		mutex_lock(&parent_mutex);
-		//while(worms > 0)		
-		//cond_wait(cv_empty, &parent_mutex);
-		//critical section
 		printf("I'm a parent bird: %d\n", worms);
 		worms = wormsCopy;
-		//cond_signal(cv_worms);
 		mutex_unlock(&baby_mutex);
-		//mutex_unlock(&parent_mutex);
-		//sleep(1000);
+		
 		parent_itr--;
 	}
-	//kill(getpid());
 }
 
