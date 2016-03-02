7cb05d1c63b8cd9870ef3701814e0e448a1ee2b9
Suprith Suvarneshwar <suprchan@indiana.edu>
2016-03-01 19:22:55 -0500

    final commit with report

--- apps/babyBird.c
+++ apps/babyBird.c
@@ -4,24 +4,38 @@
 
 
 void babyBirdProcess(int babyId){	
-	while(1){
+	volatile int myshare = eat_share;
+	//int status = 0;	
+	while(myshare > 0){
 		
 		mutex_lock(&baby_mutex); //do nothing, wait for the lock to be false
 		
 		//wait here if worms are less than or equal to zero
 		if(worms <= 0)
 			cond_wait(cond_worms, &baby_mutex);
-		worms--;
-		printf("Baby bird %d ate a worm, worm count: %d\n", babyId, worms);
 		
-		if(worms <= 0){
+	
+		if(worms > 0){		
+			worms--;
+			myshare--;
+			printf("Baby bird %d ate a worm, worm count: %d\n", babyId, worms);
+		
+			if(myshare > 0 && worms <= 0){
+			//		printf("signalling by baby bird %d\n", babyId);
+					cond_signal(cond_empty);
 			
-			cond_signal(cond_empty);
+			}
+		}
+		if(worms <= 0){
+			//		printf("signalling by baby bird %d\n", babyId);
+					cond_signal(cond_empty);
 			
 		}
-		
 		mutex_unlock(&baby_mutex); //release lock
+		
 	}
+	
+	done = done >> 1;
 	
 	
 }
--- apps/parentBird.c
+++ apps/parentBird.c
@@ -4,19 +4,19 @@
 
 
 void parentBirdProcess(){	
-	int wormsCopy = worms;	
-	while(parent_itr > 0){	
+		
+	while(done != 1){	
 		mutex_lock(&parent_mutex);
 		//wait here for the signal
 		if(worms > 0)
 			cond_wait(cond_empty, &parent_mutex);
 		
-		worms = wormsCopy;
-		printf("Parent bird filled the dish with %d worms\n", worms);
-		cond_signal(cond_worms);
+		if(worms <= 0 && done != 1){
+			worms = global_worms;
+			printf("Parent bird filled the dish with %d worms\n", global_worms);
+			cond_signal(cond_worms);
+		}
 		mutex_unlock(&parent_mutex);
-		
-		parent_itr--;
 	}
 }
 
